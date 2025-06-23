d12080a613b1e8fe2ace5b0ea66138ba51a4c349
arvind.chaurasia <arvind.chaurasia@NDLLAPARVINDKC>
2025-06-23 17:01:51 +0530

    project image path updated

--- projects/script.js
+++ projects/script.js
@@ -41,13 +41,14 @@ function getProjects() {
 
 
 function showProjects(projects) {
+    console.log('projects = ', projects);
     let projectsContainer = document.querySelector(".work .box-container");
     let projectsHTML = "";
     projects.forEach(project => {
         projectsHTML += `
         <div class="grid-item ${project.category}">
         <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="/portfolio/assets/images/projects/${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
