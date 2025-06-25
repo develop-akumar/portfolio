7e3743f7c5f7553eb573537fd416d6bc62f96a9e
arvind.chaurasia <arvind.chaurasia@NDLLAPARVINDKC>
2025-06-25 11:45:44 +0530

    exp updated

--- projects/script.js
+++ projects/script.js
@@ -42,13 +42,15 @@ function getProjects() {
 
 function showProjects(projects) {
     console.log('projects = ', projects);
+    const basePath = isLocal ? "" : "/portfolio";
+    
     let projectsContainer = document.querySelector(".work .box-container");
     let projectsHTML = "";
     projects.forEach(project => {
         projectsHTML += `
         <div class="grid-item ${project.category}">
         <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="${basePath}/assets/images/projects/${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
